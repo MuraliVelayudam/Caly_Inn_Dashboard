97d694c96466c7b629ff9083191fbe8458e980dc
murali velayudam <murali.velayudam@gmail.com>
2024-11-01 15:27:46 +0530

    fix

--- src/components/Sidebar_Components/Large_Devices/index.js
+++ src/components/Sidebar_Components/Large_Devices/index.js
@@ -2,8 +2,6 @@
 
 import Image from "next/image";
 
-import logo_white from "../../../../public/logo_white.png";
-
 import { useParams, usePathname } from "next/navigation";
 import Nav from "../Nav";
 import Logout from "@/components/Auth_Components/Logout";
@@ -16,7 +14,7 @@ export default function Large_Devices() {
     <div className="flex flex-col h-full">
       <div className="bg-black w-full h-[20vh] flex justify-center items-center rounded-t-2xl">
         <Image
-          src={logo_white}
+          src={"/logo_white.png"}
           alt="logo"
           width={150}
           height={150}
